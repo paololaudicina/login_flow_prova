aee02dcc8dacb2d7c01e1bb6f9c4c06dc878c8c4
Paolo <[paolo.laudicina@studenti.unipd.it ]>
2024-05-15 11:45:59 +0200

    login con immagine fissa ma problema dello scroll

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -4,8 +4,10 @@ import 'dart:ui';
 import 'package:ex_3/screens/datipersonali.dart';
 import 'package:ex_3/services/Impact.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
+import 'package:provider/single_child_widget.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'home.dart';
 import 'package:flutter/material.dart';
@@ -35,170 +37,173 @@ class _LoginPageState extends State<LoginPage> {
     }
     
     final _formKey = GlobalKey<FormState>();
-    return Scaffold(
+    return SafeArea(
             
-            body: Stack(
-              children: [
-                Container(
-                  height: double.infinity,
-                  width: double.infinity,
-                  decoration: const BoxDecoration(
-                    gradient: LinearGradient(colors: [
-                       Color.fromARGB(255, 2, 71, 129),
-                       Color.fromARGB(255, 172, 198, 243)
-                    ])
-
-                  ),
-                  
-                  child:  const Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.all(70),
-                        child: CircleAvatar(backgroundImage: AssetImage('images/login-image.jpg'),
-                        radius: 80),
-                      ),
-                    ],
-                  )
-                  ),
-
-              SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 300,bottom: 50),
-                  child: Container(
-                    height: 450,
-                    decoration:const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(40.0),
-                      topLeft: Radius.circular(40.0),
-                      bottomLeft: Radius.circular(40.0),
-                      bottomRight: Radius.circular(40.0)
-                  )
-                  ),
+            child: Stack(
+              children: 
+                 [
+                  Container(
+                    height: double.infinity,
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(colors: [
+                        Color.fromARGB(255, 25, 22, 234),
+                        Color.fromARGB(255, 124, 140, 232)
+                      ])
                     
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [ const Padding(
-                        padding:  EdgeInsets.all(40),
-                        child: Text(
-
-                          'Welcome User',
-                          style: TextStyle(color: Colors.blue,
-                          
-                          fontSize: 30),
-                          textAlign:TextAlign.center
+                                
+                    ),
+                    
+                    child: Image.asset('images/welcome.jpeg', fit: BoxFit.cover)
+                    ),
+              
+                Scaffold(
+                  backgroundColor: Colors.transparent,
+                  body: SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 100,bottom: 100),
+                      child: Container(
+                        height: 350,
+                        decoration:const BoxDecoration(
+                        color: Colors.transparent,
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(40.0),
+                          topLeft: Radius.circular(40.0),
+                          bottomLeft: Radius.circular(40.0),
+                          bottomRight: Radius.circular(40.0)
+                      )
+                      ),
+                        
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [ const Padding(
+                            padding:  EdgeInsets.all(20),
+                            child: Text(
+                                  
+                              'Welcome User',
+                              style: TextStyle(color: Colors.black,
+                              
+                              fontSize: 35),
+                              textAlign:TextAlign.center
+                            ),
+                          ),
+                            Form(
+                              key: _formKey,
+                              child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children:[
+                                  Padding(
+                                   padding: const  EdgeInsets.all(20.0),
+                                   child:  TextFormField(
+                                    
+                                    
+                                     validator: (String? value) {
+                                      if (value == null || value.isEmpty ){
+                                        return 'inserisci un username';
+                                                      
+                                      }else {
+                                        return null;
+                                        }
+                                                      
+                                     },
+                                     style: const TextStyle(color: Colors.black),
+                                     controller: userController,
+                                    textAlign: TextAlign.start,
+                                    decoration: const InputDecoration(
+                                      border: OutlineInputBorder(),
+                                      label: Text('Username',style: TextStyle(color: Colors.black)),
+                                      
+                                      
+                                      prefixIcon: Icon(Icons.person,color: Colors.black),
+                                      
+                                      
+                                    ),
+                                    
+                                    ),
+                                 ),
+                                  Padding(
+                                   padding:  const EdgeInsets.all(20.0),
+                                   child:  TextFormField(
+                                    validator: (String? value) {
+                                      if ( value == null || value.isEmpty ){
+                                        return 'inserisci una password';
+                                                      
+                                      }else {
+                                        return null;
+                                      }
+                                    },
+                                     controller: passwordController,
+                                     style: const TextStyle(color: Colors.black),
+                                    textAlign: TextAlign.start,
+                                    obscureText: _passwordView,
+                                    decoration:  InputDecoration(
+                                      border: const OutlineInputBorder(),
+                                      label: const Text('Password',style: TextStyle(color: Colors.black)),
+                                      suffixIcon: IconButton(
+                                        onPressed: _chenageViewPassword,
+                                        icon: Icon(_passwordView ? Icons.visibility_off : Icons.visibility, color: Colors.black,)
+                                        
+                                      ),
+                                      prefixIcon: const Icon(Icons.key,color: Colors.black,)
+                                      
+                                    ),
+                                    ),
+                                 ),
+                              
+                                Container(
+                                  decoration:BoxDecoration(
+                              
+                                    borderRadius: BorderRadius.circular(20)
+                                  ),
+                                  height: 50,
+                                  width: 250,
+                                  child: ElevatedButton(onPressed: () async{
+                                    if (_formKey.currentState!.validate()){
+                                      final response = await Impact().getAndStoreToken(userController.text, passwordController.text);
+                                      if (response == 200){
+                                        final sp = await SharedPreferences.getInstance();
+                                        sp.setString('username', userController.text);
+                                        sp.setString('password',passwordController.text);
+                                        bool flag = true;
+                                        await sp.setBool('flag', flag );
+                                        if (sp.getBool('DatiPersonali') != null) {
+                                          
+                                          Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) =>  const home()));
+                                       }else {
+                                          Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) =>   DatiPersonali()));
+                                       }
+                                      }
+                                      
+                                    } else {
+                                       ScaffoldMessenger.of(context)
+                                    ..removeCurrentSnackBar()
+                                    ..showSnackBar(const SnackBar(content: Text('Wrong username or password')));
+                                    }
+                                       
+                                    }, 
+                                    style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll(Color.fromARGB(255, 97, 41, 226))),
+                                    
+                                    child:const Text('Login', style: TextStyle(color: Colors.white))
+                                    ),
+                                )
+                                ],
+                                
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                        Form(
-                          key: _formKey,
-                          child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children:[
-                              Padding(
-                               padding: const  EdgeInsets.all(20.0),
-                               child:  TextFormField(
-                                
-                                 validator: (String? value) {
-                                  if (value == null || value.isEmpty ){
-                                    return 'inserisci un username';
-                                                  
-                                  }else {
-                                    return null;
-                                    }
-                                                  
-                                 },
-                                 controller: userController,
-                                textAlign: TextAlign.start,
-                                decoration: const InputDecoration(
-                                  border: OutlineInputBorder(),
-                                  labelText: 'username',
-                                  prefixIcon: Icon(Icons.person)
-                                  
-                                ),
-                                
-                                ),
-                             ),
-                              Padding(
-                               padding:  EdgeInsets.all(20.0),
-                               child:  TextFormField(
-                                validator: (String? value) {
-                                  if ( value == null || value.isEmpty ){
-                                    return 'inserisci una password';
-                                                  
-                                  }else {
-                                    return null;
-                                  }
-                                },
-                                 controller: passwordController,
-                                textAlign: TextAlign.start,
-                                obscureText: _passwordView,
-                                decoration:  InputDecoration(
-                                  border: const OutlineInputBorder(),
-                                  labelText: 'Password',
-                                  suffixIcon: IconButton(
-                                    onPressed: _chenageViewPassword,
-                                    icon: Icon(_passwordView ? Icons.visibility_off : Icons.visibility)
-                                    
-                                  ),
-                                  prefixIcon: const Icon(Icons.key)
-                                  
-                                ),
-                                ),
-                             ),
-                          
-                            Container(
-                              decoration:BoxDecoration(
-                          
-                                borderRadius: BorderRadius.circular(20)
-                              ),
-                              height: 50,
-                              width: 250,
-                              child: ElevatedButton(onPressed: () async{
-                                if (_formKey.currentState!.validate()){
-                                  final response = await Impact().getAndStoreToken(userController.text, passwordController.text);
-                                  if (response == 200){
-                                    final sp = await SharedPreferences.getInstance();
-                                    sp.setString('username', userController.text);
-                                    sp.setString('password',passwordController.text);
-                                    bool flag = true;
-                                    await sp.setBool('flag', flag );
-                                    if (sp.getBool('DatiPersonali') != null) {
-                                      
-                                      Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) =>  const home()));
-                                   }else {
-                                      Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) =>   DatiPersonali()));
-                                   }
-                                  }
-                                  
-                                } else {
-                                   ScaffoldMessenger.of(context)
-                                ..removeCurrentSnackBar()
-                                ..showSnackBar(const SnackBar(content: Text('Wrong username or password')));
-                                }
-                                   
-                                }, 
-                                
-                                child:const Text('Login')
-                                ),
-                            )
-                            ],
-                            
-                          ),
-                        ),
-                      ],
                     ),
                   ),
                 ),
-              ),
+                
+                ]
               
-              ]
-            
-            ),
-          );   
+              ),
+            );
+             
   }
 }
 
